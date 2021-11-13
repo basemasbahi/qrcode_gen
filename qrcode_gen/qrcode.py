8eb452b355d24261e8d5df23f63d927fa42da94e
ayman nasser <aymenit2008@gmail.com>
2021-11-13 12:52:07 +0000

    for arabic qr

--- qrcode_gen/qrcode.py
+++ qrcode_gen/qrcode.py
@@ -6,6 +6,7 @@ import pyqrcode
 
 @frappe.whitelist()
 def gen_qrcode(text):
-    data = pyqrcode.create(text)
+    txt = text.encode('utf-8', 'replace').decode('latin-1')
+    data = pyqrcode.create(txt)
 
     return f'data:image/png;base64,{data.png_as_base64_str(scale=5)}'
